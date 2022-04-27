98407829193bac294345331c1e8c3f9bec2a7a0f
ZuzaBedkowska <zbedkowska@student.agh.edu.pl>
2022-04-27 14:13:02 +0200

    Poprawienie generowania kółka

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <cmath>
 #include <math.h>
+#include<windows.h>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
@@ -20,16 +21,16 @@ void makeCircle(int n, GLfloat vertices[], GLuint indices[], double x, double y,
 		i++;
 		vertices[i] = 0.0;
 		i++;
-		vertices[i] = r * cos(angle1); //drugi wierzcholek z f trygonometrycznych
+		vertices[i] = x + r * cos(angle1); //drugi wierzcholek z f trygonometrycznych
 		i++;
-		vertices[i] = r * sin(angle1);
+		vertices[i] = y + r * sin(angle1);
 		i++;
 		vertices[i] = 0.0;
 		i++;
 		angle1 += angle;
-		vertices[i] = r * cos(angle1); //trzeci wierzcholek tak samo jak 2, ale przesuniety o kat
+		vertices[i] = x + r * cos(angle1); //trzeci wierzcholek tak samo jak 2, ale przesuniety o kat
 		i++;
-		vertices[i] = r * sin(angle1);
+		vertices[i] = y + r * sin(angle1);
 		i++;
 		vertices[i] = 0.0;
 	}
@@ -38,6 +39,8 @@ void makeCircle(int n, GLfloat vertices[], GLuint indices[], double x, double y,
 		indices[i] = i; //pierwszy wierzcholek kazdego trojkata to srodek kola
 	}
 }
+
+
 
 int main()
 {
@@ -85,25 +88,6 @@ int main()
 	glLinkProgram(shaderProgram);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-	GLfloat vertices[3 * 3 * 100]{}; //100 trojkatow po 3 punkty po 3 wsp
-	// Indices for vertices order
-	GLuint indices[3 * 100]{};
-	makeCircle(n, vertices, indices, 0.0, 0.0, 1.0);
-	GLuint VAO, VBO, EBO; //nowa wersja trojkata
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
 	//koniec trojkata
 
 	//petla wyswietlajaca
@@ -111,6 +95,24 @@ int main()
 	GLfloat blue = 255.0;
 	while (!glfwWindowShouldClose(window))
 	{
+		GLfloat vertices[3 * 3 * 100]{}; //100 trojkatow po 3 punkty po 3 wsp
+		// Indices for vertices order
+		GLuint indices[3 * 100]{};
+		makeCircle(n, vertices, indices, 0.5, 0.5, 0.05);
+		GLuint VAO, VBO, EBO; //nowa wersja trojkata
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
@@ -118,15 +120,14 @@ int main()
 		glDrawElements(GL_TRIANGLES, 3 * n * n, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteBuffers(1, &EBO);
+		glDeleteProgram(shaderProgram);
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteProgram(shaderProgram);
 	}
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
-	glDeleteProgram(shaderProgram);
-
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteProgram(shaderProgram);
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
 	// Terminate GLFW before ending the program
